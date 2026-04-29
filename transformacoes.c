7b863d5a636b121b76d0665088f8fcb6221c8200
Fabrici01 <fabricioamaral504@gmail.com>
2026-04-29 16:38:36 -0300

    mod transformacoes

--- transformacoes.c
+++ transformacoes.c
@@ -1,10 +1,8 @@
-void rotacao(float a11, float a12, float a21, float a22, float *x, float *y){
-    float a31, a32;
-    a31 = (-(*x)*a11) - (*y)*a21;
-    a32 = (-(*x)*a12) - (*y)*a22;
-    
-    *x = (*x)*a11 + (*y)*a21 + a31 + (*x);
-    *y = (*x)*a12 + (*y)*a22 + a32 + (*y);
+#include <math.h>
+
+void rotacao(float anguloRotacao, float *x, float *y){
+    float cos, sen;
+
 }
 
 void translacao(float transX, float transY, float *x, float *y){
--- transformacoes.h
+++ transformacoes.h
@@ -1,3 +1,3 @@
-void rotacao(float a11, float a12, float a21, float a22, float *x, float *y);
+void rotacao(float anguloRotacao, float *x, float *y);
 void translacao(float transX, float transY, float *x, float *y);
 void escala(float multX, float multY, float *x, float *y);
